075751eb00732c3c8bf149e100602beb46089181
Narciso Correa <don@MacBook-Pro-3.T-mobile.com>
2018-07-23 22:08:16 -0400

    added ability to edit and delete

--- spending/urls.py
+++ spending/urls.py
@@ -7,11 +7,16 @@ from . import views
 urlpatterns = [
     path('', views.view_all_spending, name ='view_all_spending' ),
     path('spending/',views.SpendingView.as_view(), name='spending'),
-    path('vendors/', views.VendorsView.as_view(),name = 'vendors'),
     path('add_spend/',views.add_spend),
-    path('edit_spend/<int:spending_id>/', views.edit_spend),
+	path('edit_spend/<int:spending_id>/', views.edit_spend),
+	path('delete_spend/<int:spending_id>', views.delete_spend),
+    path('vendors/', views.VendorsView.as_view(),name = 'vendors'),
     path('add_vendor/',views.add_vendor),
+    path('edit_vendor/<int:vendor_id>/', views.edit_vendor),
+    path('delete_vendor/<int:vendor_id>/', views.delete_vendor),
     path('add_category/',views.add_category),
+    path('edit_category/<int:category_id>/', views.edit_category),
+    path('delete_category/<int:category_id>',views.delete_category),    
     path('categories/',views.CategoriesView.as_view(), name='categories'),
 
 ]
--- spending/views.py
+++ spending/views.py
@@ -30,15 +30,10 @@ def add_spend(request):
 
 def edit_spend(request, spending_id):
 	instance = get_object_or_404(Spending, id = spending_id)
-	#a = Spending.objects.get(pk=spending_id)
 	form = AddSpendingForm(request.POST or None,instance=instance)
-	print(form.instance.id	)
 	if request.method == 'POST':
-
-		#form = AddSpendingForm(request.POST)
 		if form.is_valid():
 			spend = form.save(commit=False)
-			spend.spend_id = spending_id
 			spend.spend_vendor = Vendors.objects.get(pk= request.POST.get('spend_vendor'))
 			spend.spend_date = datetime.now()
 			spend.spend_desc = request.POST.get('spend_desc')
@@ -52,6 +47,10 @@ def edit_spend(request, spending_id):
 		#print(form)
 	return render(request, 'spending/edit_spend.html',{'form':form})
 
+def delete_spend(request,spending_id):
+	instance = get_object_or_404(Spending,id=spending_id)
+	instance.delete()
+	return HttpResponseRedirect('/spending/')
 
 def add_vendor(request):
 	if request.method == "POST":
@@ -64,6 +63,28 @@ def add_vendor(request):
 	else:
 		form = AddVendorsForm()
 	return render(request, 'spending/add_vendor.html',{'form':form})
+
+def edit_vendor(request, vendor_id):
+	instance = get_object_or_404(Vendors, id=vendor_id)
+	form = AddVendorsForm(request.POST or None,instance=instance)
+	if request.method == "POST":
+		if form.is_valid():
+			vendor = form.save(commit=False)
+			vendor.vendor_name = request.POST.get('vendor_name')
+			vendor.save()
+		return HttpResponseRedirect('/vendors/')
+	else:
+		form = AddVendorsForm(instance=instance)
+	return render(request, 'spending/edit_vendor.html',{'form':form})
+
+
+def delete_vendor(request, vendor_id):
+	instance = get_object_or_404(Vendors, id=vendor_id)
+	instance.delete()
+	return HttpResponseRedirect('/vendors/')
+
+
+
 
 def add_category(request):
 	if request.method == "POST":
@@ -80,6 +101,31 @@ def add_category(request):
 def view_all_spending(request):
 
 	return HttpResponse('This is the landing page')
+
+
+def edit_category(request, category_id):
+	instance = get_object_or_404(SpendCategory,pk=category_id)
+	form = AddCategoryForm(request.POST or None, instance=instance)
+		
+	if request.method == "POST":
+		if form.is_valid():
+			cat = form.save(commit=False)
+			cat.category_name = request.POST.get('category_name')
+			cat.save()
+		return HttpResponseRedirect('/categories/')
+	else:
+		form = AddCategoryForm(instance=instance)
+	return render(request, 'spending/edit_category.html', {'form':form})
+
+def delete_category(request, category_id):
+	instance = get_object_or_404(SpendCategory,id=category_id)
+	instance.delete()
+	return HttpResponseRedirect('/categories/')
+
+def view_all_spending(request):
+
+	return HttpResponse('This is the landing page')
+
 
 
 class SpendingView(generic.ListView):
